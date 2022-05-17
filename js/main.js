dc551be9138fe3f5ab865eefa16cdf23d287b2e8
Edoardo22 <vanaliedoardo@gmail.com>
2022-05-17 18:36:11 +0200

    finito corretto

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ const nomeUtente = prompt("Qual è il tuo nome?");
 const cognomeUtente = prompt("Quale è il tuo cognome?");
 const colorePreferito = prompt("Quale è il tuo colore preferito?");
 const nomeCognomeColore22 = document.getElementById("NomeCognomeColore22");
-nomeCognomeColore22.innerHTML = (nomeUtente + cognomeUtente + colorePreferito + 22);
+nomeCognomeColore22.innerHTML = (nomeUtente + cognomeUtente + colorePreferito + "22");
